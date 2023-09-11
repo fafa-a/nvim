5071abbbb6ab598551c6dac432837e2ff67a66c4
fafa-a <fabienpuech1@gmail.com>
2023-09-11 19:33:23 +0200

    chore: add rainbow

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,24 @@
+return{"HiPhish/rainbow-delimiters.nvim",
+  config= function ()
+   vim.g.rainbow_delimiters = {
+    strategy = {
+        [''] = rainbow_delimiters.strategy['global'],
+        vim = rainbow_delimiters.strategy['local'],
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+    },
+    highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+    },
+}
+ 
+  end
+}
