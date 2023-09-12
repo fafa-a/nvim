09e88d890c71cbfc638cf1bdc631fe99019da077
PUECH Fabien <fabien.puech@csgroup.eu>
2023-09-12 09:34:38 +0200

    fix : var in rainbow

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,10 +1,10 @@
 return{"HiPhish/rainbow-delimiters.nvim",
   config= function ()
    vim.g.rainbow_delimiters = {
-    strategy = {
-        [''] = rainbow_delimiters.strategy['global'],
-        vim = rainbow_delimiters.strategy['local'],
-    },
+    -- strategy = {
+    --     [''] = rainbow_delimiters.strategy['global'],
+    --     vim = rainbow_delimiters.strategy['local'],
+    -- },
     query = {
         [''] = 'rainbow-delimiters',
         lua = 'rainbow-blocks',
